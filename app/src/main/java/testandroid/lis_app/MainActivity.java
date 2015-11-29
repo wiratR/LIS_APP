c02cdcdecb2bc2156e2a722a8c5dfb6e96ebe53c
dorabon <wirateds@gmail.com>
2015-11-29 21:25:19 +0700

    Cretae DATABASE USER

--- app/src/main/java/testandroid/lis_app/MainActivity.java
+++ app/src/main/java/testandroid/lis_app/MainActivity.java
@@ -11,22 +11,24 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private UserTABLE objUserTABLE;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        //Connect SQLite
+        connectedSQLite();
+
+    }// onCreate
+
+    private void connectedSQLite() {
+
+        objUserTABLE = new UserTABLE(this);
+    }// conectedSQLite
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
